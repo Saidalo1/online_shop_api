f82d375e8c362a22d17770e19d5411fa85d78a56
Saidalo <super.saydalo@mail.ru>
2022-09-18 09:42:37 +0500

    updates n

--- env/Lib/site-packages/admin_volt/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminVoltConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_volt'
--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Saida'lo\Desktop\ComputerSparePartShop\env
+set VIRTUAL_ENV=C:\Users\Admin\Desktop\ComputerSparePartShop\env
 
 if not defined PROMPT set PROMPT=$P$G
 
