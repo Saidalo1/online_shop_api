1407a97da1223b919fc244b05ce24d6cd707e390
Saidalo <50578058+Saidalo1@users.noreply.github.com>
2022-12-09 20:05:32 +0500

    Updates temporarily

--- root/settings.py
+++ root/settings.py
@@ -79,14 +79,18 @@ AUTH_USER_MODEL = 'users.User'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'csp_shop',
-        'USER': 'postgres',
-        'PASSWORD': '123098',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'csp_shop',
+#         'USER': 'postgres',
+#         'PASSWORD': '123098',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'cspshop',
+            }
 }
 
 # Password validation
